f865d295790bf76150f1da98ec9f17db8fe8ebce
Tamarawillis44 <techietl444@gmail.com>
2022-09-07 22:36:29 -0400

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Tamara laraque on 9/7/22.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
